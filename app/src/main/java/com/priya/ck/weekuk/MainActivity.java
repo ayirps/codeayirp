01de3dd001993a931f10b8a90e69eda639b44fd2
ayirps <code.ayirp@gmail.com>
2018-08-02 01:16:47 +0530

    Initial commit

---
+++ app/src/main/java/com/priya/ck/weekuk/MainActivity.java
@@ -0,0 +1,55 @@
+package com.priya.ck.weekuk;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Boolean firstTime = null;
+    private static int SPLASH_TIME = 3000; //3 seconds
+
+    /**
+     * Checks if the user is opening the app for the first time.
+     * Note that this method should be placed inside an activity and it can be called multiple times.
+     * @return boolean
+     */
+    private boolean isFirstTime() {
+        if (firstTime == null) {
+            SharedPreferences mPreferences = this.getSharedPreferences("first_time", Context.MODE_PRIVATE);
+            firstTime = mPreferences.getBoolean("firstTime", true);
+            if (firstTime) {
+                SharedPreferences.Editor editor = mPreferences.edit();
+                editor.putBoolean("firstTime", false);
+                editor.commit();
+            }
+        }
+        return firstTime;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Show tutorial only on very first launch of App after installation
+        if (true/*isFirstTime()*/){
+            //Code to start timer and take action after the timer ends
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //Do any action here. Now we are moving to next page
+                    Intent mySuperIntent = new Intent(MainActivity.this, tutorialactivity.class);
+                    startActivity(mySuperIntent);
+                    /* This 'finish()' is for exiting the app when back button pressed */
+                    finish();
+                }
+            }, SPLASH_TIME);
+        }else{
+
+        }
+    }
+}
---
+++ app/src/main/java/com/priya/ck/weekuk/tutorialactivity.java
@@ -0,0 +1,208 @@
+package com.priya.ck.weekuk;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class tutorialactivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnNext;
+    private Timer timer;
+    //private PrefManager prefManager;
+
+    private class SliderTimer extends TimerTask {
+
+        @Override
+        public void run() {
+            tutorialactivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (viewPager.getCurrentItem() < layouts.length - 1) {
+                        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                    } else {
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_tutorial);
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        btnNext = (Button) findViewById(R.id.btn_next);
+
+        // layouts of all welcome sliders
+        // add few more layouts if you want
+        layouts = new int[]{
+                R.layout.tutorialscreen1,
+                R.layout.tutorialscreen2,
+                R.layout.tutorialscreen3};
+
+        // adding bottom dots
+        //addBottomDots(0);
+
+        // making notification bar transparent
+        //changeStatusBarColor();
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener); //Added for manual scrolling
+
+        //Code for Automatic scrolling of tutorial
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new SliderTimer(), 2000, 3000);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking for last page
+                // if last page home screen will be launched
+                int current = getItem(+1);
+                if (current < layouts.length) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);
+                } else {
+                    timer.cancel(); //stop autoscroll on case 1: button click at last tutorial page
+                    //launchLoginScreen();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        timer.cancel(); //stop autoscroll
+        super.onDestroy();
+    }
+
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[layouts.length];
+
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+    }
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    /*private void launchHomeScreen() {
+        //prefManager.setFirstTimeLaunch(false);
+        startActivity(new Intent(tutorialactivity.this, MainActivity.class));
+        finish();
+    }*/
+
+    //  viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            //addBottomDots(position);
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == layouts.length - 1) {
+                // last page. make button text to GetStarted
+                btnNext.setText(getString(R.string.start));
+                timer.cancel(); //stop autoscroll on case 2: Autoscroll of slides
+            } else {
+                // still pages are left
+                btnNext.setText(getString(R.string.next));
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+    /**
+     * Making notification bar transparent
+     */
+   /* private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }*/
+
+    /**
+     * View pager adapter
+     */
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+}
+
+
+
